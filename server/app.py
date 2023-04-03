27c1a2ab35f4f50a7df3fbfba43f0283c00c7d4d
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-04-03 15:50:57 -0400

    learning new tricks on routes

--- server/app.py
+++ server/app.py
@@ -4,5 +4,39 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return'<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print (param)
+    return f'{param}'
+
+@app.route('/count/<int:number>')
+def count_parameter(number): 
+    output = ''
+    for i in range(0, number):
+        output += str(i) + '\n'
+    return output
+
+# A math() view should take three parameters: num1, operation, and num2. It must perform the appropriate operation on the two numbers in the order that they are presented. The included operations should be: +, -, *, div (/ would change the URL path), and %. Its URL should be of the format /math/<num1><operation><num2>
+
+@app.route('/math/<int:num1><string:operation><int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation =='*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return 'Invalid Operation'
+    return str(result)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
